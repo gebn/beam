234282acbe0c365ccc20fbaea78a29f783df23ae
George Brighton <george@gebn.co.uk>
2016-12-05 05:22:51 +0000

    Make Resource tests more maintainable

--- beam/tests/test_resource.py
+++ beam/tests/test_resource.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from __future__ import unicode_literals, division
 import unittest
 
 from beam.resource import Resource
@@ -7,20 +7,34 @@ from beam.resource import Resource
 
 class TestResource(unittest.TestCase):
 
+    _USED_BYTES = 100
+    _FREE_BYTES = 50
+
+    @classmethod
+    def setUpClass(cls):
+        cls.resource = Resource(cls._USED_BYTES, cls._FREE_BYTES)
+
     def test_used_bytes(self):
-        self.assertEqual(Resource(100, 50).used_bytes, 100)
+        self.assertEqual(self.resource.used_bytes, self._USED_BYTES)
 
     def test_free_bytes(self):
-        self.assertEqual(Resource(100, 50).free_bytes, 50)
+        self.assertEqual(self.resource.free_bytes, self._FREE_BYTES)
 
     def test_total_bytes(self):
-        self.assertEqual(Resource(100, 50).total_bytes, 150)
+        self.assertEqual(self.resource.total_bytes,
+                         self._USED_BYTES + self._FREE_BYTES)
 
     def test_used_percentage(self):
-        self.assertAlmostEqual(Resource(100, 50).used_percentage, 0.666, 2)
+        self.assertAlmostEqual(
+            self.resource.used_percentage,
+            self._USED_BYTES / (self._USED_BYTES + self._FREE_BYTES),
+            2)
 
     def test_free_percentage(self):
-        self.assertAlmostEqual(Resource(100, 50).free_percentage, 0.333, 2)
+        self.assertAlmostEqual(
+            self.resource.free_percentage,
+            1 - self._USED_BYTES / (self._USED_BYTES + self._FREE_BYTES),
+            2)
 
     def test_percentages_sum_to_one(self):
         resource = Resource(14682693209, 522188218791)
@@ -56,16 +70,16 @@ class TestResource(unittest.TestCase):
                                                 '6728904704,48'))
 
     def test_eq_false_class(self):
-        self.assertNotEqual(22, Resource(100, 50))
+        self.assertNotEqual(22, self.resource)
 
     def test_eq_false_used(self):
-        self.assertNotEqual(Resource(100, 50), Resource(50, 50))
+        self.assertNotEqual(self.resource, Resource(50, 50))
 
     def test_eq_false_free(self):
-        self.assertNotEqual(Resource(100, 50), Resource(100, 100))
+        self.assertNotEqual(self.resource, Resource(100, 100))
 
     def test_eq_true(self):
-        self.assertEqual(Resource(100, 50), Resource(100, 50))
+        self.assertEqual(self.resource, self.resource)
 
     def test_str(self):
         resource = Resource(6155997184, 6728904704)
