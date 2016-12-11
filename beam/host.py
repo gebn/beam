d5d7679232d31d05f4a93bff9d662c7af6f1763e
George Brighton <george@gebn.co.uk>
2016-12-11 19:15:05 +0000

    Split out Host into Host and HostIdentity in preparation for config

--- beam/host.py
+++ beam/host.py
@@ -9,7 +9,73 @@ from beam.resource import Resource
 
 
 @six.python_2_unicode_compatible
-class Host(object):
+class HostIdentity(object):
+    """
+    Represents identification information for a host.
+    """
+
+    @property
+    def request_params(self):
+        """
+        Retrieve parameters that identify this host, suitable for inclusion in
+        an API request.
+
+        :return: The host parameters dictionary.
+        """
+        return {'key': self.key, 'hash': self.hash}
+
+    def __init__(self, name, key, hash_, vendor):
+        """
+        Initialise a new host identity object.
+
+        :param name: The friendly name of the node.
+        :param key: The node's key.
+        :param hash_: The node's hash.
+        :param vendor: The vendor providing this host.
+        """
+        self.name = name
+        self.key = key
+        self.hash = hash_
+        self.vendor = vendor
+
+    def __hash__(self):
+        """
+        Retrieve a hash value for this object.
+
+        :return: This object's hash. Identical objects will have an identical
+                 hash.
+        """
+        return hash(self.hash)
+
+    def __eq__(self, other):
+        """
+        Test whether this host is identical to another.
+
+        :param other: The object to compare to this one.
+        :return: True if the objects are identical, false otherwise.
+        """
+        print(other.hash)
+        print(self.hash)
+        # could use key xor hash here
+        return isinstance(other, self.__class__) and \
+            other.hash == self.hash
+
+    def __str__(self):
+        """
+        Generate a human-readable string representation of this host.
+
+        :return: This host as a friendly string.
+        """
+        return '{0}({1}, {2})'.format(self.__class__.__name__,
+                                      self.name,
+                                      self.vendor.name)
+
+
+@six.python_2_unicode_compatible
+class Host(HostIdentity):
+    """
+    Represents a host with all its metadata.
+    """
 
     @property
     def is_offline(self):
@@ -20,14 +86,12 @@ class Host(object):
         """
         return not self.is_online
 
-    def __init__(self, name, key, hash_, fqdn, primary_ip, is_online,
-                 memory, storage, bandwidth, ip_addresses):
+    def __init__(self, identity, fqdn, primary_ip, is_online, memory, storage,
+                 bandwidth, ip_addresses):
         """
         Initialise a new host object.
 
-        :param name: The friendly name of the node.
-        :param key: The node's key.
-        :param hash_: The node's hash.
+        :param identity: This host's identity.
         :param fqdn: The node's fully-qualified domain name (sans trailing .).
         :param primary_ip: The node's primary IP(v4/v6) address.
         :param is_online: Whether the node is up.
@@ -36,9 +100,8 @@ class Host(object):
         :param bandwidth: Bandwidth used and remaining.
         :param ip_addresses: All IP addresses assigned to the node.
         """
-        self.name = name
-        self.key = key
-        self.hash = hash_
+        super(Host, self).__init__(identity.name, identity.key, identity.hash,
+                                   identity.vendor)
         self.fqdn = fqdn
         self.primary_ip = primary_ip
         self.is_online = is_online
@@ -48,23 +111,21 @@ class Host(object):
         self.ip_addresses = ip_addresses
 
     @classmethod
-    def from_response(cls, response, name, key, hash_):
+    def from_response(cls, body, identity):
         """
         Create a host object from an API response.
 
-        :param response: The raw API response text.
-        :param name: The host's alias.
-        :param key: The host's key.
-        :param hash_: The host's hash.
+        :param body: The raw API response text.
+        :param identity: This host's identity.
         :return: An object representing the host.
         :raises ValueError: If the response is empty or malformed.
         :raises RuntimeError: If the response indicates the API request failed.
         """
-        if not response:
+        if not body:
             raise ValueError('Cannot construct host from empty response')
 
         try:
-            root = cElementTree.fromstring('<root>' + response + '</root>')
+            root = cElementTree.fromstring('<root>' + body + '</root>')
 
             if root.find('status').text != 'success':
                 message = root.find('statusmsg')
@@ -72,7 +133,8 @@ class Host(object):
                     'Response indicates failed API call: {0}'.format(
                         message.text if message else 'unspecified error'))
 
-            return Host(name, key, hash_, root.find('hostname').text,
+            return Host(identity,
+                        root.find('hostname').text,
                         root.find('ipaddress').text,
                         root.find('vmstat').text == 'online',
                         Resource.from_response(root.find('mem').text),
@@ -84,18 +146,6 @@ class Host(object):
         except AttributeError as e:
             raise ValueError(
                 'Host response is missing an attribute: {0}'.format(e))
-
-    def __eq__(self, other):
-        """
-        Test whether this host is identical to another.
-
-        :param other: The object to compare to this one.
-        :return: True if the objects are identical, false otherwise.
-        """
-        # could use key xor hash here
-        return isinstance(other, self.__class__) and \
-            other.key == self.key and \
-            other.hash == self.hash
 
     def __str__(self):
         """
--- beam/tests/test_host.py
+++ beam/tests/test_host.py
@@ -3,14 +3,48 @@ from __future__ import unicode_literals
 import unittest
 
 from beam.resource import Resource
-from beam.host import Host
+from beam.host import Host, HostIdentity
+from beam.vendor import Vendor
+
+
+class TestHostIdentity(unittest.TestCase):
+
+    _NAME = 'name'
+    _KEY = 'key'
+    _HASH = 'hash'
+    _VENDOR = Vendor('vendor-name', 'vendor-endpoint')
+
+    @classmethod
+    def setUpClass(cls):
+        cls.identity = HostIdentity(cls._NAME, cls._KEY, cls._HASH, cls._VENDOR)
+        cls.other_identity = HostIdentity(cls._NAME, cls._KEY, 'h', cls._VENDOR)
+
+    def test_request_pams(self):
+        self.assertDictEqual(self.identity.request_params,
+                             {'key': self._KEY, 'hash': self._HASH})
+
+    def test_hash_match(self):
+        self.assertEqual(hash(self.identity), hash(self.identity))
+
+    def test_hash_different(self):
+        self.assertNotEqual(hash(self.other_identity), hash(self.identity))
+
+    def test_eq_false_class(self):
+        self.assertNotEqual(22, self.identity)
+
+    def test_eq_false_hash(self):
+        self.assertNotEqual(self.other_identity, self.identity)
+
+    def test_str(self):
+        self.assertEqual(str(self.identity),
+                         'HostIdentity({0}, {1})'.format(self._NAME,
+                                                         self._VENDOR.name))
 
 
 class TestHost(unittest.TestCase):
-    
-    _NAME = 'this-is-the-name'
-    _KEY = 'this-is-the-key'
-    _HASH = 'this-is-the-hash'
+    _VENDOR_ENDPOINT = 'https://vpscp.ramnode.com'
+    IDENTITY = HostIdentity('host-name', 'host-key', 'host-hash',
+                            Vendor('vendor-name', _VENDOR_ENDPOINT))
     _FQDN = 'host.example.com'
     _PRIMARY_IP = '8.8.8.8'
     _IS_ONLINE = True
@@ -27,20 +61,26 @@ class TestHost(unittest.TestCase):
 <hostname>{2}</hostname>
 <ipaddress>{3}</ipaddress>
 <vmstat>{4}</vmstat>'''
-    
+    _XML_VALID = _XML.format(','.join(_IP_ADDRESSES),
+                             'success',
+                             _FQDN,
+                             _PRIMARY_IP,
+                             'online' if _IS_ONLINE else 'offline')
+    HOST = Host(IDENTITY, _FQDN, _PRIMARY_IP, _IS_ONLINE, _MEMORY, _STORAGE,
+                _BANDWIDTH, _IP_ADDRESSES)
+
     @classmethod
     def setUpClass(cls):
-        cls.host = Host(cls._NAME, cls._KEY, cls._HASH, cls._FQDN,
-                        cls._PRIMARY_IP, cls._IS_ONLINE, cls._MEMORY,
-                        cls._STORAGE, cls._BANDWIDTH, cls._IP_ADDRESSES)
+        cls.host = Host(cls.IDENTITY, cls._FQDN, cls._PRIMARY_IP,
+                        cls._IS_ONLINE, cls._MEMORY, cls._STORAGE,
+                        cls._BANDWIDTH, cls._IP_ADDRESSES)
 
     @staticmethod
-    def _make_host(name=_NAME, key=_KEY, hash_=_HASH, fqdn=_FQDN,
-                   primary_ip=_PRIMARY_IP, is_online=_IS_ONLINE, memory=_MEMORY,
-                   storage=_STORAGE, bandwidth=_BANDWIDTH,
-                   ip_addresses=_IP_ADDRESSES):
-        return Host(name, key, hash_, fqdn, primary_ip, is_online, memory,
-                    storage, bandwidth, ip_addresses)
+    def _make_host(identity=IDENTITY, fqdn=_FQDN, primary_ip=_PRIMARY_IP,
+                   is_online=_IS_ONLINE, memory=_MEMORY, storage=_STORAGE,
+                   bandwidth=_BANDWIDTH, ip_addresses=_IP_ADDRESSES):
+        return Host(identity, fqdn, primary_ip, is_online, memory, storage,
+                    bandwidth, ip_addresses)
 
     def test_is_offline_false(self):
         self.assertFalse(self.host.is_offline)
@@ -50,54 +90,30 @@ class TestHost(unittest.TestCase):
 
     def test_from_response_none(self):
         with self.assertRaises(ValueError):
-            Host.from_response(None, self._NAME, self._KEY, self._HASH)
+            Host.from_response(None, self.IDENTITY)
 
     def test_from_response_malformed(self):
         with self.assertRaises(ValueError):
-            Host.from_response('><', self._NAME, self._KEY,
-                               self._HASH)
+            Host.from_response('><', self.IDENTITY)
 
     def test_from_response_api_failure(self):
         with self.assertRaises(RuntimeError):
             Host.from_response(self._XML.format(','.join(self._IP_ADDRESSES),
-                               'failure',
-                               self._FQDN,
-                               self._PRIMARY_IP,
-                               'online' if self._IS_ONLINE else 'offline'),
-                               self._NAME,
-                               self._KEY,
-                               self._HASH)
+                                                'failure',
+                                                self._FQDN,
+                                                self._PRIMARY_IP,
+                                                'online' if self._IS_ONLINE
+                                                else 'offline'),
+                               self.IDENTITY)
 
     def test_from_response_missing_attribute(self):
         with self.assertRaises(ValueError):
-            Host.from_response('<root/>', self._NAME, self._KEY,
-                               self._HASH)
+            Host.from_response('<root/>', self.IDENTITY)
 
     def test_from_response(self):
         self.assertEqual(
-            Host.from_response(self._XML.format(','.join(self._IP_ADDRESSES),
-                               'success',
-                               self._FQDN,
-                               self._PRIMARY_IP,
-                               'online' if self._IS_ONLINE else 'offline'),
-                               self._NAME,
-                               self._KEY,
-                               self._HASH),
+            Host.from_response(self._XML_VALID, self.IDENTITY),
             self.host)
-
-    def test_eq_false_class(self):
-        self.assertNotEqual(22, self.host)
-
-    def test_eq_false_key(self):
-        self.assertNotEqual(self._make_host(hash_=self._KEY + 'changed'),
-                            self.host)
-
-    def test_eq_false_hash(self):
-        self.assertNotEqual(self._make_host(hash_=self._HASH + 'changed'),
-                            self.host)
-
-    def test_eq_true(self):
-        self.assertEqual(self.host, self.host)
 
     def test_str(self):
         self.assertEqual(str(self.host),
